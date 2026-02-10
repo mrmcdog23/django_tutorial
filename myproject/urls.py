96434c8e5eb370a6b243179d14afd2f315a19c1e
mrmcdog23 <joeleveson@hotmail.com>
2026-02-10 11:24:43 +0000

    new commit

--- myproject/urls.py
+++ myproject/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
@@ -26,3 +28,6 @@ urlpatterns = [
     path('posts/', include('posts.urls')),
     path('users/', include('users.urls'))
 ]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
